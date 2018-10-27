c218e34c0fc72c2d7ff4c2246b7f78a15c5a1743
Daniel.Zhuang <daniel.zhuang@ef.com>
2018-10-27 18:34:05 +0800

    update to use batch

--- translate.py
+++ translate.py
@@ -5,9 +5,14 @@ translator = translate.Client()
 
 file_input="work/source.md"
 file_output="work/translated.md"
+translate_batch_size = 50
 
 def require_translate(content):
     return content and not content.startswith("[") and not content.startswith("!")
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 with codecs.open(file_input,'r',encoding='utf8') as fi:
     content = fi.readlines()
@@ -21,13 +26,13 @@ with codecs.open(file_input,'r',encoding='utf8') as fi:
     for line in content:
         source_items.append(line);
 
-    for item in source_items:
-        if(require_translate(item)):
-            task_items.append(item)
-
-    translations = translator.translate(source_items,target_language='zh-cn')
-    for translation in translations:
-        translated_items.append(translation['translatedText'])
+    # split into batches
+    source_item_chunks = chunks(source_items, translate_batch_size)
+    for source_item_chunk in source_item_chunks:
+        # do translate
+        translations = translator.translate(source_item_chunk,target_language='zh-cn')
+        for translation in translations:
+            translated_items.append(translation['translatedText'])
 
     # write to output
     i = 0
